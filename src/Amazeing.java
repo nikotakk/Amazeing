e88e76c54ceb561a17a899baff3f23f40e0e9481
Niko Takkinen <nikotakk@gmail.com>
2016-03-11 01:41:15 +0200

    Added instruction screen when starting the game.

--- src/Amazeing.java
+++ src/Amazeing.java
@@ -14,20 +14,21 @@ public class Amazeing extends JFrame {
     private int movebarHeight = 130;
     private int mazeSize = 25*16;
 
+
     public Amazeing() {
 
         initUI();
     }
     // Initializing the UI.
     private void initUI() {
-        /* Luodaan menubar, johon pistetään menuja. */
+        /* Creating a menubar, to which menus are added.. */
         JMenuBar menubar = new JMenuBar();
         JMenu file = new JMenu("File");
         JMenu help = new JMenu("Help");
         file.setMnemonic(KeyEvent.VK_F);
         help.setMnemonic(KeyEvent.VK_H);
 
-        /* Luodaan Exit menuItem ja pistetään se menubariin. */
+        /*Creating Exit menuItem and inserting it into menubar. */
         JMenuItem menuExit= new JMenuItem("Exit");
         menuExit.setMnemonic(KeyEvent.VK_E);
         menuExit.setToolTipText("Exit application");
@@ -43,17 +44,20 @@ public class Amazeing extends JFrame {
         menubar.add(file);
         menubar.add(help);
         setJMenuBar(menubar);
-        /* Testataa vertikaalista JToolBaria oikealla. */
+
+        /* Testing a vertical JToolBari on the right side of the window. */
         JToolBar vertical = new JToolBar(JToolBar.VERTICAL);
         vertical.setFloatable(false);
         vertical.setMargin(new Insets(0, infobarWidth/2, 0, infobarWidth/2));
-        add(vertical, BorderLayout.EAST);
-        /* Testataa JLabelia alhaalla. */
+
+        /* Testing JLabel on the south side of the window. */
         JLabel statusbar = new JLabel();
         statusbar.setPreferredSize(new Dimension(-1, movebarHeight));
         /* statusbar.setBorder(LineBorder.createGrayLineBorder()); */
-        add(statusbar, BorderLayout.SOUTH);
 
+        add(vertical, BorderLayout.EAST);
+        add(statusbar, BorderLayout.SOUTH);
+        /* Adding a board to the center of the window. */
         add(new Board(), BorderLayout.CENTER);
 
         setSize(mazeSize+infobarWidth+4, mazeSize+movebarHeight+21);
@@ -66,13 +70,10 @@ public class Amazeing extends JFrame {
     /* Main. */
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+        EventQueue.invokeLater(() -> {
 
-                Amazeing game = new Amazeing();
-                game.setVisible(true);
-            }
+            Amazeing game = new Amazeing();
+            game.setVisible(true);
         });
     }
 }
--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,13 @@
 
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import java.awt.font.GraphicAttribute;
+import javax.swing.*;
+
 /**
  * Created by nigel on 2/26/16.
  */
@@ -20,6 +18,11 @@ public class Board extends JPanel implements ActionListener {
     private Map map;
     private Ball ball;
     private final int DELAY = 10;
+    private boolean inGame = false;
+    private int blockSize = 25;
+    private int screenSize = blockSize*16;
+    private Image instructions;
+
 
     public Board() {
 
@@ -31,6 +34,7 @@ public class Board extends JPanel implements ActionListener {
         addKeyListener(new TAdapter());
         setFocusable(true);
         setBackground(Color.BLACK);
+        inGame = false;
 
         map = new Map();
         ball = new Ball();
@@ -39,20 +43,35 @@ public class Board extends JPanel implements ActionListener {
 
     }
 
-    //Painting components.
+
+
+    // Painting components.
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawMap(g);
-        drawBall(g);
+        if (!inGame) {
+            showIntroScreen(g);
+        }
+        else {
+            drawMap(g);
+            drawBall(g);
 
-        Toolkit.getDefaultToolkit().sync();
+            Toolkit.getDefaultToolkit().sync();
+        }
     }
+
+    // Drawing instructions screen.
+    private void showIntroScreen(Graphics g) {
+        ImageIcon ii = new ImageIcon("src/resources/instructions.png");
+        instructions = ii.getImage();
+        g.drawImage(instructions,1,1,this);
+    }
+
     // Drawing the balls graphics.
     private void drawBall(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(ball.getImage(), ball.getX() * 25, ball.getY() * 25, this);
+        g2d.drawImage(ball.getImage(), ball.getX() * blockSize, ball.getY() * blockSize, this);
     }
     // Drawing the maps graphics.
     private void drawMap(Graphics g) {
@@ -60,10 +79,10 @@ public class Board extends JPanel implements ActionListener {
         for (int y = 0; y < 16; y++) {
             for (int x = 0; x < 16; x++) {
                 if (map.getMap(x,y).equals("g")) {
-                    g.drawImage(map.getGround(), x * 25, y * 25, null);
+                    g.drawImage(map.getGround(), x * blockSize, y * blockSize, null);
                 }
                 if (map.getMap(x,y).equals("w")) {
-                    g.drawImage(map.getWall(), x * 25, y * 25, null);
+                    g.drawImage(map.getWall(), x * blockSize, y * blockSize, null);
                 }
             }
         }
@@ -81,58 +100,64 @@ public class Board extends JPanel implements ActionListener {
 
             int key = e.getKeyCode();
 
-            if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
-                if ( !map.getMap( ball.getX() -1, ball.getY() ).equals("w") ) {
-                    ball.move(-1, 0);
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+                    if (!map.getMap(ball.getX() - 1, ball.getY()).equals("w")) {
+                        ball.move(-1, 0);
+                    }
+                    System.out.println("Left or A pressed.");
                 }
-                System.out.println("Left or A pressed.");
+
+                if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
+                    if (!map.getMap(ball.getX() + 1, ball.getY()).equals("w")) {
+                        ball.move(1, 0);
+                    }
+                    System.out.println("Right or D pressed.");
+                }
+
+                if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
+                    if (!map.getMap(ball.getX(), ball.getY() - 1).equals("w")) {
+                        ball.move(0, -1);
+                    }
+                    System.out.println("Up or W pressed.");
+                }
+
+                if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
+                    if (!map.getMap(ball.getX(), ball.getY() + 1).equals("w")) {
+                        ball.move(0, 1);
+                    }
+                    System.out.println("Down or S pressed.");
+                }
+                if (key == KeyEvent.VK_ESCAPE) {
+                    System.out.println("Esc pressed.");
+                    if (timer.isRunning()) {
+                        timer.stop();
+                    } else {
+                        timer.start();
+                    }
+//                System.exit(1);
+                }
+            } else {
+                if (key == KeyEvent.VK_G) {
+                    inGame = true;
+                    System.out.println("G pressed");
+                    //initGame();
+                }
             }
 
-            if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
-                if ( !map.getMap( ball.getX() +1, ball.getY() ).equals("w") ) {
-                    ball.move(1, 0);
-                }
-                System.out.println("Right or D pressed.");
-            }
-
-            if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
-                if ( !map.getMap( ball.getX(), ball.getY() -1 ).equals("w") ) {
-                    ball.move(0, -1);
-                }
-                System.out.println("Up or W pressed.");
-            }
-
-            if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
-                if ( !map.getMap( ball.getX(), ball.getY() +1 ).equals("w") ) {
-                    ball.move(0, 1);
-                }
-                System.out.println("Down or S pressed.");
-            }
-            if (key == KeyEvent.VK_ESCAPE) {
-                System.out.println("Esc pressed.");
-                System.exit(1);
-            }
         }
 
         public void keyReleased(KeyEvent e) {
 
             int key = e.getKeyCode();
 
-            if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+            if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A
+                    || key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S
+                    || key == KeyEvent.VK_UP || key == KeyEvent.VK_W
+                    || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
                 ball.move(0,0);
             }
 
-            if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
-                ball.move(0,0);
-            }
-
-            if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
-                ball.move(0,0);
-            }
-
-            if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
-                ball.move(0,0);
-            }
         }
     }
 }
