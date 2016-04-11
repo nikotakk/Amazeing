c5b5292e75aaf4ea4e24b70593d4f9911a6bdbe0
Niko Takkinen <nikotakk@gmail.com>
2016-04-11 11:42:37 +0300

    Added nonfunctional highscore and level info.

--- src/Amazeing.java
+++ src/Amazeing.java
@@ -21,102 +21,18 @@ public class Amazeing extends JFrame {
 
     public Amazeing() {
 
+        initMenuBar();
         initUI();
         watch();
     }
-    // Initializing the UI.
-    private void initUI() {
-
-        initMenuBar();
-
-        /* Testing JLabel on the south side of the window. */
-        JLabel statusBar = new JLabel();
-        statusBar.setLayout(new BoxLayout(statusBar, BoxLayout.X_AXIS));
-        statusBar.setPreferredSize(new Dimension(mazeSize, movebarHeight));
-        statusBar.setBackground(Color.LIGHT_GRAY);
-        statusBar.setBorder(LineBorder.createBlackLineBorder());
-
-        /* Creating a box of buttons into the statusBar. */
-        JPanel buttonsBox = new JPanel();
-        buttonsBox.setLayout(new GridBagLayout());
-        JButton up = new JButton("^");
-        JButton left = new JButton("<");
-        JButton down = new JButton("v");
-        JButton right = new JButton(">");
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        buttonsBox.add(up, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        buttonsBox.add(down, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        buttonsBox.add(left, gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 1;
-        buttonsBox.add(right, gbc);
-//TODO: Move the ball when clicking the buttons.
-//        buttonsBox.setBorder(LineBorder.createBlackLineBorder());
-
-        /* Adding best time, current time and current lvl into the statusBar. */
-        JPanel infoBox = new JPanel();
-        infoBox.setAlignmentX(1f);
-        infoBox.setLayout(new BoxLayout(infoBox, BoxLayout.Y_AXIS));
-//        infoBox.setBorder(LineBorder.createGrayLineBorder());
-
-        infoBox.add(new JLabel("Level: 1"));
-        infoBox.add(new JLabel("Highscore: "));
-
-        time = new JLabel();
-        time.setPreferredSize(new Dimension(75,25));
-//        time.setBorder(LineBorder.createBlackLineBorder());
-
-        statusBar.add(infoBox);
-        statusBar.add(time);
-//        statusBar.add(Box.createHorizontalGlue());
-//        statusBar.add(Box.createRigidArea(new Dimension(5, 0)));
-//        statusBar.add(time);
-//        statusBar.add(Box.createHorizontalGlue());
-//        statusBar.add(Box.createRigidArea(new Dimension(110, 0)));
-        statusBar.add(buttonsBox);
-
-        add(statusBar, BorderLayout.SOUTH);
-
-        /* Adding a board to the center of the window. */
-        board = new Board();
-        add(board, BorderLayout.CENTER);
-
-        setSize(mazeSize, mazeSize+movebarHeight+21);
-        setResizable(false);
-        setTitle("Amazeing");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public void watch () {
-        Thread watch = new Thread() {
-
-            public void run() {
-                while (true) {
-                    long milliSeconds = board.getTime("millis") - board.getTime("seconds")*100;
-                    time.setText("Time:" + board.getTime("seconds") + "." + milliSeconds);
-                }
-            }
-        };
-
-        watch.start();
-    }
-
+    /* Initializing the menuBar. */
     private void initMenuBar() {
 
-        /* Creating a menuBar, to which menus File and Help are added. */
+        /* Creating a menuBar, to which menus Game and Help are added. */
         JMenuBar menuBar = new JMenuBar();
-        JMenu file = new JMenu("File");
+        JMenu game = new JMenu("Game");
         JMenu help = new JMenu("Help");
-        file.setMnemonic(KeyEvent.VK_F);
+        game.setMnemonic(KeyEvent.VK_A);
         help.setMnemonic(KeyEvent.VK_H);
 
         /*Creating Reset menuItem. */
@@ -152,14 +68,101 @@ public class Amazeing extends JFrame {
             }
         });
 
-        /* Addin Exit and Reset into File, Instructions into Help, and adding them all into menuBar. */
-        file.add(menuReset);
-        file.add(menuExit);
+        /* Adding Exit and Reset into Game, Instructions into Help, and adding them all into menuBar. */
+        game.add(menuReset);
+        game.addSeparator();
+        game.add(menuExit);
         help.add(menuInstructions);
-        menuBar.add(file);
+        menuBar.add(game);
         menuBar.add(help);
         setJMenuBar(menuBar);
 
+    }
+
+    /* Initializing the UI. */
+    private void initUI() {
+
+        /* statusBar JLabel on the south side of the window. */
+        JLabel statusBar = new JLabel();
+        statusBar.setLayout(new BoxLayout(statusBar, BoxLayout.X_AXIS));
+        statusBar.setPreferredSize(new Dimension(mazeSize, movebarHeight));
+        statusBar.setBackground(Color.LIGHT_GRAY);
+        statusBar.setBorder(LineBorder.createBlackLineBorder());
+
+        /* Creating a box of buttons to be put into the statusBar. */
+        JPanel buttonsBox = new JPanel();
+        buttonsBox.setLayout(new GridBagLayout());
+        JButton up = new JButton("^");
+        JButton left = new JButton("<");
+        JButton down = new JButton("v");
+        JButton right = new JButton(">");
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        buttonsBox.add(up, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        buttonsBox.add(down, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        buttonsBox.add(left, gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        buttonsBox.add(right, gbc);
+//TODO: Move the ball when clicking the buttons.
+//        buttonsBox.setBorder(LineBorder.createBlackLineBorder());
+
+        /* Adding best time, current time and current lvl into the statusBar. */
+        JPanel infoBox = new JPanel();
+        infoBox.setAlignmentX(1f);
+        infoBox.setLayout(new BoxLayout(infoBox, BoxLayout.Y_AXIS));
+        infoBox.setPreferredSize(new Dimension(150,50));
+//        infoBox.setBorder(LineBorder.createGrayLineBorder());
+
+        infoBox.add(new JLabel("Level: 1"));
+        infoBox.add(new JLabel("Highscore: "));
+
+        time = new JLabel();
+        time.setPreferredSize(new Dimension(75,25));
+//        time.setBorder(LineBorder.createBlackLineBorder());
+
+        statusBar.add(Box.createRigidArea(new Dimension(5, 0)));
+        statusBar.add(infoBox);
+        statusBar.add(time);
+        statusBar.add(buttonsBox);
+//        statusBar.add(Box.createHorizontalGlue());
+//        statusBar.add(time);
+//        statusBar.add(Box.createHorizontalGlue());
+//        statusBar.add(Box.createRigidArea(new Dimension(110, 0)));
+
+        add(statusBar, BorderLayout.SOUTH);
+
+        /* Adding a board to the center of the window. */
+        board = new Board();
+        add(board, BorderLayout.CENTER);
+
+        setSize(mazeSize, mazeSize+movebarHeight+21);
+        setResizable(false);
+        setTitle("Amazeing");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /* The stopwatch to be used in the game. */
+    public void watch () {
+        Thread watch = new Thread() {
+
+            public void run() {
+                while (true) {
+                    long milliSeconds = board.getTime("millis") - board.getTime("seconds")*100;
+                    time.setText("Time:" + board.getTime("seconds") + "." + milliSeconds);
+                }
+            }
+        };
+
+        watch.start();
     }
 
     /* Main. */
